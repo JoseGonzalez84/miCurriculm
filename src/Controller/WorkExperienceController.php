16548e5c99969fe6ed84b3aa260cac0344283cea
Jose Gonzalez <josegs84@gmail.com>
2023-07-07 11:30:59 +0200

    Modify development location and added some visual changes

--- src/Controller/WorkExperienceController.php
+++ src/Controller/WorkExperienceController.php
@@ -30,21 +30,37 @@ class WorkExperienceController extends AbstractController
                 'title' => 'Desarrollador fullstack',
                 'company' => 'Artica ST',
                 'companyLink' => 'https://pandorafms.com/es/',
-                'fromTo' => '2019/2023',
-                'description' => 'Pues hacía un poco el mono',
+                'companyLogo' => 'img/companies/articast.jfif',
+                'fromTo' => [
+                    'from' => 'Junio de 2019',
+                    'to' => 'Marzo de 2023'
+                ],
+                'description' => 'Mi principal cometido era la resolución de bugs y desarrollo de mejoras de producto. El
+principal lenguaje utilizado era PHP nativo en su versión 8.0. Seguido de JavaScript en conjunto
+a jQuery y en algunas ocasiones con librería React. Cuando me centraba en la parte backend,
+se trabajaba con PHP y Perl. Utilizaba tecnologías de desarrollo como Docker y GitLab. La
+empresa al estar en Madrid, trabajé 100% en modalidad remota.',
+                'headTechnologies' => ['PHP', 'Perl', 'JavaScript'],
+                'headSkills' => ['Backend', 'Frontend', 'Maitenance', 'New features', 'Full Remote']
             ],
             [
                 'title' => 'Desarrollador .NET',
                 'company' => 'Everis',
                 'companyLink' => 'https://es.nttdata.com/',
-                'fromTo' => '2018/2019',
+                'fromTo' => [
+                    'from' => 'Septiembre de 2018',
+                    'to' => 'Junio de 2019'
+                ],
                 'description' => 'Mis jefes eran poco simpaticos',
             ],
             [
                 'title' => 'Picateclas Jr',
                 'company' => 'Artica ST',
                 'companyLink' => 'https://www.linkedin.com/company/atramentum-sl/',
-                'fromTo' => '2017/2018',
+                'fromTo' => [
+                    'from' => 'Julio de 2017',
+                    'to' => 'Septiembre de 2018'
+                ],
                 'description' => 'Mucho escribir pero poco producir',
             ]
         ];
